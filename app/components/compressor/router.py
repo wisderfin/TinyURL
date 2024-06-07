09a2be6b88bffb132ec27179292737e75cf184a5
wisderfin <kassas20@mail.ru>
2024-06-07 16:27:21 +0400

    add point '/compressor/add' for add and compressing links, and save in db

--- app/components/compressor/router.py
+++ app/components/compressor/router.py
@@ -9,11 +9,15 @@ from app.base.database import get_async_session
 router = APIRouter(prefix="/compressor")
 
 
-@router.post("/")
+@router.post("/add")
 async def add(link: AddLinkScheme, session: AsyncSession = Depends(get_async_session)):
     latest_note = await get_latest_url(session)
-    short = next_short(latest_note.short)
-    next_id = latest_note.id + 1
+    if latest_note is None:
+        short = 'AAAAAAAA'
+        next_id = 1
+    else:
+        short = next_short(latest_note.short)
+        next_id = latest_note.id + 1
     result = await add_link(full=link.full, short=short, next_id=next_id, session=session)
     return result
 
--- app/components/compressor/utils.py
+++ app/components/compressor/utils.py
@@ -6,18 +6,19 @@ from app.components.compressor.schemes import LinkScheme
 
 
 # добавление ссылки в бд
-async def add_link(short: str, full: AnyHttpUrl, next_id: int, session: AsyncSession):
+async def add_link(short: str, full: AnyHttpUrl, next_id: int, session: AsyncSession) -> dict:
     new_link = LinkModel(id=next_id, full=str(full), short=short)
     session.add(new_link)
     await session.commit()
     return new_link.__dict__
 
 
-async def get_latest_url(session: AsyncSession) -> LinkScheme:
+async def get_latest_url(session: AsyncSession) -> LinkScheme | None:
     latest = select(LinkModel).order_by(desc(LinkModel.id))
     latest = await session.execute(latest)
     latest = latest.scalars().first()
-    latest_dict = dict(latest.__dict__)
-    latest_dict.pop('full', AnyHttpUrl(latest_dict['full']))
-    # latest_dict.pop('_sa_instance_state', None)
-    return LinkScheme(**latest_dict)
+    if latest is not None:
+        latest_dict = dict(latest.__dict__)
+        return LinkScheme(**latest_dict)
+    return None
+
